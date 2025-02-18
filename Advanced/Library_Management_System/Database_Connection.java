6aa36aa383dfedc8b12af285ab4d4c2e72ec3ae7
Mohammed_Rizwan <160338674+TheNew-WebDeveloper@users.noreply.github.com>
2025-02-18 15:45:41 +0530

    Create Database_Connection.java

---
+++ Advanced/Library_Management_System/Database_Connection.java
@@ -0,0 +1,16 @@
+package Advanced.Library_Management_System;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Database_Connection {
+
+    private static final String URL = "jdbc:mysql://localhost:3306/library_management";
+    private static final String USER = "root"; // MySQL username
+    private static final String PASSWORD = "Rizwan@786"; // MySQL password
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
