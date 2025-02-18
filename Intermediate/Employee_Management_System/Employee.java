de11487e1ad09a9e6fd9041d1674699bbc019377
Mohammed_Rizwan <160338674+TheNew-WebDeveloper@users.noreply.github.com>
2025-02-18 15:39:13 +0530

    Create Employee.java

---
+++ Intermediate/Employee_Management_System/Employee.java
@@ -0,0 +1,44 @@
+package Intermediate.Employee_Management_System;
+
+class Employee {
+    private String name;
+    private int id;
+    private double salary;
+
+    // Constructor
+    public Employee(String name, int id, double salary) {
+        this.name = name;
+        this.id = id;
+        this.salary = salary;
+    }
+
+    // Getters and Setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    // Method to display employee details
+    public void display() {
+        System.out.println("ID: " + id + ", Name: " + name + ", Salary: " + salary);
+    }
+}
