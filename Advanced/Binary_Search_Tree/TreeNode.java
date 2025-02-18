82b7ec3411ae0fe15b4a6e7c905f0b416f01b851
Mohammed_Rizwan <160338674+TheNew-WebDeveloper@users.noreply.github.com>
2025-02-18 15:48:50 +0530

    Create TreeNode.java

---
+++ Advanced/Binary_Search_Tree/TreeNode.java
@@ -0,0 +1,11 @@
+package assignments.Binary_Search_Tree;
+
+class TreeNode {
+    int data;
+    TreeNode left, right;
+
+    public TreeNode(int data) {
+        this.data = data;
+        left = right = null;
+    }
+}
