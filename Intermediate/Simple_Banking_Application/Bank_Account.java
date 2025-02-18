487179c2ca6418eb259b9b0874341322bac3f43d
Mohammed_Rizwan <160338674+TheNew-WebDeveloper@users.noreply.github.com>
2025-02-18 15:43:22 +0530

    Create Bank_Account.java

---
+++ Intermediate/Simple_Banking_Application/Bank_Account.java
@@ -0,0 +1,44 @@
+package Intermediate.Banking;
+
+class BankAccount {
+    private double balance;
+
+    // Constructor to initialize balance
+    public BankAccount(double initialBalance) {
+        if (initialBalance >= 0) {
+            this.balance = initialBalance;
+        } else {
+            this.balance = 0;
+            System.out.println("Initial balance cannot be negative. Setting balance to 0.");
+        }
+    }
+
+    // Deposit money into the account
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+            System.out.println("Deposited: $" + amount);
+        } else {
+            System.out.println("Deposit amount must be positive.");
+        }
+    }
+
+    // Withdraw money from the account
+    public void withdraw(double amount) {
+        if (amount > 0) {
+            if (amount <= balance) {
+                balance -= amount;
+                System.out.println("Withdrew: $" + amount);
+            } else {
+                System.out.println("Insufficient funds. Current balance: $" + balance);
+            }
+        } else {
+            System.out.println("Withdrawal amount must be positive.");
+        }
+    }
+
+    // Check the current balance
+    public void checkBalance() {
+        System.out.println("Current balance: $" + balance);
+    }
+}
